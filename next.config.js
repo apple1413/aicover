cc31cdbc447877590e43960c732f62412b27b72e
apple1413 <604097514@qq.com>
2025-01-13 21:38:57 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'fataicover.s3.us-east-2.amazonaws.com'
+    ],
+  },
+}
+
+module.exports = nextConfig 
